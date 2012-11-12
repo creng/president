86da5ec8c3bde1400bd75f017c82145c683aadaf
Kevin <Kevin@Kev.local>
2012-11-12 00:12:35 -0500

    Added utils.js that contains the basic building blocks of the game.

---
+++ utils.js
@@ -0,0 +1,206 @@
+// ------------------
+//      HELPERS
+// ------------------
+
+var ranks = [
+	'Two', 'Three', 'Four', 
+	'Five', 'Six', 'Seven', 
+	'Eight', 'Nine', 'Ten', 
+	'Jack', 'Queen', 'King', 'Ace'
+];
+
+var suits = [
+	'Spades',
+	'Hearts',
+	'Clubs',
+	'Diamonds'
+];
+
+function sortCards(a, b){
+	if ( a.value < b.value ) { return -1; }
+	else if ( a.value > b.value ) { return 1; }
+	else { return 0; }
+}
+
+function validateHand (rank, hand) {
+	var handRank = hand[0].rank;
+	for (var i = 0; i < hand.length; i++) {
+		if ( hand[i].rank != handRank || hand[i].rank < rank){
+			return false;
+		}
+	}
+	return true;
+}
+
+// ------------------
+//       CARD
+// ------------------
+
+function Card (num) {
+	this.value = num;
+	this.rank = ranks[Math.floor(num / 4)];
+	this.suit = suits[num % 4];
+}
+
+Card.prototype.toString = function () {
+	return ( this.rank + ' of ' + this.suit );
+}
+
+Card.prototype.toValue = function () {
+	return this.value;
+}
+
+// ------------------
+//       DECK
+// ------------------
+
+function Deck () {
+	this.size = 52;
+	this.cards = [];
+	for (var i = 0; i < this.size; i++) {
+		this.cards[i] = new Card(i);
+	}
+}
+
+Deck.prototype.shuffle = function () {
+	var size = this.size;
+	if ( size === 0 ) { return; }
+	while ( --size ) {
+		var temp = Math.floor( Math.random() * (size + 1) );
+		var tempCard = this.cards[ temp ];
+		this.cards[temp] = this.cards[size];
+		this.cards[size] = tempCard;
+	}
+}
+
+Deck.prototype.deal = function () {
+	var card = this.cards[0];
+	this.cards.splice(0, 1);
+	this.size--;
+	return card;
+}
+
+Deck.prototype.toString = function () {
+	var msg = '### Deck Begin ###\n';
+	for (var i = 0; i < this.size; i++){
+		msg += this.cards[i].toString() + '\n';
+	}
+	return ( msg += '### Deck End ###' );
+}
+
+// ------------------
+//      PLAYER
+// ------------------
+
+function Player (name) {
+	this.name = name;
+	this.hand = [];
+}
+
+Player.prototype.addHand = function (hand) {
+	this.hand = this.hand.concat(hand);
+}
+
+Player.prototype.sortHand = function () {
+	this.hand.sort(sortCards);
+}
+
+// ------------------
+//       TABLE
+// ------------------
+
+function Table () {
+	this.size = 0;
+	this.cards = [];
+	this.currentCards = [];
+}
+
+Table.prototype.addHand = function (hand) {
+	this.size += hand.length;
+	this.cards = this.cards.concat(hand);
+	this.currentCards = hand;
+}
+
+Table.prototype.clear = function (player) {
+	player.addHand(this.cards);
+	this.currentCards = [];
+	this.cards = [];
+	this.size = 0;
+}
+
+// ------------------
+//       GAME
+// ------------------
+
+function Game (players) {
+	this.players = players;
+	this.deck = new Deck();
+	this.table = new Table();
+	this.currentPlayer = 0;
+}
+
+Game.prototype.deal = function () {
+	this.deck.shuffle();
+	for (var i = 0; i < this.deck.size; i++) {
+		this.players[i % this.players.length].addHand( [ this.deck.cards[i] ] );
+	}
+	for (var i = 0; i < this.players.length; i++) {
+		this.players[i].sortHand();
+	}
+}
+
+// Add cards to the table and update current player
+Game.prototype.turn = function (hand) {
+	var currentCards = this.table.currentCards;
+	var rank;
+	if ( currentCards.length > 0 ){
+		if ( currentCards.length != hand.length ) { return false; }
+	}
+
+	if ( hand.length > 4 || hand.length < 1) { return false; }
+
+	if ( validateHand(currentCards[0].rank, hand) ) {
+		this.table.addHand(hand);
+		this.players[this.currentPlayer].hand.splice(
+			this.players[this.currentPlayer].hand.indexOf(hand[0]), 1
+		);
+		this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
+		return true;
+	}
+	else { return false; }
+}
+
+var Kevin = new Player('Kevin');
+var Alex = new Player('Alex');
+var Ed = new Player('Ed');
+var Joan = new Player('Joan');
+
+var players = [Kevin, Alex, Ed, Joan];
+
+var x = new Game(players);
+x.deal();
+console.log(Kevin);
+x.turn([ Kevin.hand[0] ]);
+console.log(x.table);
+console.log(Kevin);
+
+// console.log(Kevin);
+// console.log(Alex);
+// console.log(Ed);
+// console.log(Joan);
+
+
+
+
+
+// var d = new Deck();
+
+// for (var i = 0; i < 52; i++){
+// 	var x = d.deal();
+// 	console.log(x.toString());
+// 	console.log(d.size);
+// }
+
+// console.log(d.toString());
+// d.shuffle();
+// console.log(d.toString());
